38a0dd9ae093b88f61d37827e9276e01efb14a3c
Heber <parzival.html@gmail.com>
2024-08-16 22:07:50 +0200

    Increment item quantity.

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -7,18 +7,30 @@ export default function useOrder(){
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item: MenuItem) => {
-    
-        /**
-         * Casting de parámetro recibido MenuItem a OrderItem
-         * Creamos una nueva variable con la copia del objeto recibido por parámetro y le añadimos la propiedad 'quantity'
-         * para que el nuevo objeto 'newItem' pueda ser del type OrderItem
-         */
-        const newItem = {...item, quantity: 1}
-        /**
-         * Spread operator para copiar los valores del state actual y añadimos el nuevo objeto
-         */
-        setOrder([...order, newItem])
+
+        /** Evitar duplicados al añadir el mismo item incrementando su cantidad */
+        const itemExists = order.find(orderItem => orderItem.id === item.id)
+        if(itemExists){
+            const updatedOrder = order.map(orderItem =>
+                orderItem.id === item.id ? {...orderItem, quantity: orderItem.quantity + 1} : orderItem
+            )
+            setOrder(updatedOrder)
+
+        } else {
+            /**
+             * Casting de parámetro recibido MenuItem a OrderItem
+             * Creamos una nueva variable con la copia del objeto recibido por parámetro y le añadimos la propiedad 'quantity'
+             * para que el nuevo objeto 'newItem' pueda ser del type OrderItem
+             */
+            const newItem = {...item, quantity: 1}
+            /**
+             * Spread operator para copiar los valores del state actual y añadimos el nuevo objeto
+             */
+            setOrder([...order, newItem])
+        }
     }
+
+    console.log(order)
 
     return {
         addItem
